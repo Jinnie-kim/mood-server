b8f0ed1220cc596d722429a3701e89cc0627cd2d
Jinnie <hjk7639@gmail.com>
2023-04-03 19:09:06 +0900

    스포티파이 auth server 생성

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const SpotifyWebApi = require('spotify-web-api-node');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+app.post('/login', (req, res) => {
+  const code = req.body.code;
+
+  const spotifyApi = new SpotifyWebApi({
+    redirectUri: 'http://127.0.0.1:5173/',
+    clientId: '6727280b598f42f18646ee89b28a2568',
+    clientSecret: '7c6cc3ca31fc42c086f995cf254ca873',
+  });
+
+  spotifyApi
+    .authorizationCodeGrant(code)
+    .then((data) => {
+      res.json({
+        accessToken: data.body.access_token,
+        refreshToken: data.body.refresh_token,
+        expiresIn: data.body.expires_in,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.sendStatus(400);
+    });
+});
+
+app.listen(5174);
