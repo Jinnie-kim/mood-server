157492e898b0b30ed27cf2e7949fc79e4eece5a2
Jinnie <hjk7639@gmail.com>
2023-04-04 09:53:16 +0900

    스포티파이 token을 refresh하는 api 작성

--- server.js
+++ server.js
@@ -7,6 +7,29 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+app.post('/refresh', (req, res) => {
+  const refreshToken = req.body.refreshToken;
+  console.log('hi');
+  const spotifyApi = new SpotifyWebApi({
+    redirectUri: 'http://127.0.0.1:5173/',
+    clientId: '6727280b598f42f18646ee89b28a2568',
+    clientSecret: '7c6cc3ca31fc42c086f995cf254ca873',
+    refreshToken,
+  });
+
+  spotifyApi
+    .refreshAccessToken()
+    .then((data) => {
+      res.json({
+        accessToken: data.body.access_token,
+        expiresIn: data.body.expires_in,
+      });
+    })
+    .catch(() => {
+      res.sendStatus(400);
+    });
+});
+
 app.post('/login', (req, res) => {
   const code = req.body.code;
 
